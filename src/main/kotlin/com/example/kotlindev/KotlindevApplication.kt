674511ad87b2d7b3a2cac14af04cc21fcae9a40e
pyogoon <pyogoona@gmail.com>
2023-12-04 16:52:43 +0900

    init

---
+++ src/main/kotlin/com/example/kotlindev/KotlindevApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlindev
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlindevApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlindevApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlindev/KotlindevApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlindev
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlindevApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
