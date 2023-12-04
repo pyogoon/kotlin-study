e05c45921df19d6f2ec39ed38d401a580a910475
pyogoon <pyogoona@gmail.com>
2023-12-04 16:55:52 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # kotlin-study
+
 # pyogoon 
---
+++ src/main/kotlin/com/example/kotlindev/controller/Controller.kt
@@ -0,0 +1,15 @@
+package com.example.kotlindev.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class Controller {
+
+    @GetMapping("/first")
+    fun first() : ResponseEntity<String> {
+        return ResponseEntity("First OK!!", HttpStatus.OK)
+    }
+}
